cec626462cdedd90950713fb1ae7b8b8765a5e00
jhkim105 <jhkim105@gmail.com>
2019-05-15 14:00:41 +0900

    initial commit

---
+++ src/main/java/com/example/demo/DemoSbadminApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSbadminApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoSbadminApplication.class, args);
+  }
+
+}

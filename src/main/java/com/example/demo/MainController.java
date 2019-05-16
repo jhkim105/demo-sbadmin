ed7c3bc2827ae8f27f919c69b54ac00be66c3724
jhkim105 <jhkim105@gmail.com>
2019-05-16 11:00:37 +0900

    spring security login

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -4,8 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class LoginController {
+public class MainController {
   @GetMapping("/login")
   public void login() {
+  }
+
+  @GetMapping("/")
+  public String index() {
+    return "index";
   }
 }
